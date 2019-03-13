11d275ff1fe76d662c2d06cfdf9e62aaa9278396
SiDraw <sidraw.fr@gmail.com>
2019-03-13 17:04:53 +0800

    install socialite package

--- config/services.php
+++ config/services.php
@@ -44,4 +44,15 @@ return [
         ],
     ],
 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('APP_URL') . '/auth/callback/google'
+    ],
+
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect'      => env('APP_URL') . '/auth/callback/facebook'
+    ]
 ];
