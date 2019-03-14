c0f66dabd7f85e9d462a76ce6c165ef3634fd1b4
SiDraw <sidraw.fr@gmail.com>
2019-03-14 11:07:09 +0800

    add user table migration

--- database/migrations/2019_03_13_083615_create_users_table.php
+++ database/migrations/2019_03_13_083615_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -15,12 +14,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('origin_user_id');
+
+            $table->string('origin_name');
+            $table->string('display_name');
+
+            $table->string('avatar')->nullable();
+            $table->string('email')->nullable();
+
+            $table->string('provider');
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
