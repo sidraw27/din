c92129356b7396cdf43d3530eb986a9b95f48ca9
SiDraw <sidraw.fr@gmail.com>
2019-04-24 11:55:48 +0800

    update header

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge,chrome=1">
+    <meta http-equiv="content-language" content="zh-tw">
     <meta name="format-detection" content="telephone=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
