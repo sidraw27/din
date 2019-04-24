d1f0076fc8eec8c0f02709d353b7b58232195539
SiDraw <sidraw.fr@gmail.com>
2019-04-24 10:29:37 +0800

    add google analytics tag

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,6 +12,18 @@
 
     @stack('head')
 
+    @if (env('APP_ENV') === 'production')
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-138969781-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-138969781-1');
+        </script>
+    @endif
+
     <style>
         [v-cloak] {
             display: none;
